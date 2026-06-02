4b2488fa97ad878d5aa21172a805391872052c30
zasyxmi <networkasyraf@gmail.com>
2026-06-02 22:04:56 +0800

    Add technician password reset and update UI

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,12 +2,24 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        User::updateOrCreate(
+            ['email' => 'admin1@wsrms.com'],
+            [
+                'name' => 'Admin 1',
+                'phone_number' => '0123456789',
+                'password' => Hash::make('password'),
+                'role' => 'admin',
+            ]
+        );
+
         $this->call([
             UserSeeder::class,
             SparePartSeeder::class,
